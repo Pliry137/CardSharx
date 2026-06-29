0f773f652c24fc9460bdfd408ae651fd887e1042
Joe <joseph.m.neal@gmail.com>
2026-06-29 13:46:13 -0400

    Make header logo much bigger

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -10,8 +10,8 @@ const tabs = [
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-brand-ice text-brand-950 dark:bg-brand-950 dark:text-brand-ice">
-      <header className="border-b border-brand-300/30 dark:border-brand-800 px-4 py-3 flex items-center justify-center">
-        <img src={logo} alt="CardSharx" className="h-20 w-auto" />
+      <header className="border-b border-brand-300/30 dark:border-brand-800 px-4 py-4 flex items-center justify-center">
+        <img src={logo} alt="CardSharx" className="h-40 w-auto" />
       </header>
 
       <main className="flex-1 px-4 py-4 pb-20 max-w-3xl w-full mx-auto">
